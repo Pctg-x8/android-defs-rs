6ef62a9a2fb43f5b18e4712de6c798a5d2b853ae
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-07-03 21:57:10 +0900

    add: width/height getter for nativewindow

--- src/native_window.rs
+++ src/native_window.rs
@@ -1,2 +1,17 @@
 
 pub enum ANativeWindow {}
+impl ANativeWindow {
+    pub fn width(&self) -> i32 {
+        unsafe { ANativeWindow_getWidth(self as *const _ as _) }
+    }
+    pub fn height(&self) -> i32 {
+        unsafe { ANativeWindow_getHeight(self as *const _ as _) }
+    }
+}
+
+#[allow(non_snake_case)]
+extern "C" {
+    pub fn ANativeWindow_getWidth(window: *mut ANativeWindow) -> i32;
+    pub fn ANativeWindow_getHeight(window: *mut ANativeWindow) -> i32;
+    pub fn ANativeWindow_getFormat(window: *mut ANativeWindow) -> i32;
+}
